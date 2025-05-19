37343b315bd05fdf7c8a755228624999b456da9d
AmarVejapi <21w2347@bztf.ch>
2025-05-19 10:10:46 +0200

    create project 2

--- config/db.js
+++
@@ -1,7 +0,0 @@
-const Nano = require('nano');
-require('dotenv').config();
-
-const couch = Nano(process.env.COUCHDB_URL);
-const db = couch.db.use('calculations'); // Datenbankname
-
-module.exports = db;
